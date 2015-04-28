2cc6c7e005fbaa51e46353c4c7d53df7866f2eab
sugyan <sugi1982@gmail.com>
2015-04-29 01:38:04 +0900

    update calendars page

--- app/assets/javascripts/application/components/calendar_list.jsx
+++ app/assets/javascripts/application/components/calendar_list.jsx
@@ -12,7 +12,12 @@ class CalendarListElement extends React.Component {
     render() {
         return (
             <li>
-                {this.props.unitname} (<a href={"https://calendar.google.com/embed?src=" + this.props.cid} target="_blank">{this.props.summary}</a>)
+                {this.props.unitname}
+                <a href={"https://calendar.google.com/embed?src=" + this.props.cid} target="_blank">
+                    <small style={{ marginLeft: ".5em" }}>
+                        {this.props.summary}
+                    </small>
+                </a>
             </li>
         );
     }
